98c0471512030ac96559c2028512b0e1c383dae5
Symon <simon.lopez@aliar.com.co>
2022-05-16 11:39:23 -0500

    fixing

--- js/script4.js
+++ js/script4.js
@@ -1,6 +1,6 @@
 var vowel = function(str){
     var matches = str.match(/[aeiou]/gi);
-    var count1 = match1 ? match1.length: 0;
+    var count1 = matches ? matches.length: 0;
     document.getElementById('p').innerHTML = "'"+str+"' contiene "+count1+" vocal(es)"
     return false;
 };
